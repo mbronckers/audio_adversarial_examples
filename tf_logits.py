2b0359af994b5c5eac8fe5074f76c58804a71143
Max Bronckers <maxbronckers@me.com>
2020-07-19 18:40:40 +0200

    add meaningful parameters over hardcoded # + add own audio

--- tf_logits.py
+++ tf_logits.py
@@ -30,31 +30,33 @@ def compute_mfcc(audio, **kwargs):
 
     batch_size, size = audio.get_shape().as_list() # get batch size by getting audio shape
     audio = tf.cast(audio, tf.float32) # cast audio file to float32 type
+    frame_length = 512 # how many samples in a frame
+    num_mfcc_features = 26 # default n_input in DeepSpeech
 
     # 1. Pre-emphasizer, a high-pass filter: passes only signals above a cutoff frequency and attenuates lower frequencies
-    audio = tf.concat((audio[:, :1], audio[:, 1:] - 0.97*audio[:, :-1], np.zeros((batch_size,512), dtype=np.float32)), 1)
+    audio = tf.concat((audio[:, :1], audio[:, 1:] - 0.97*audio[:, :-1], np.zeros((batch_size, frame_length), dtype=np.float32)), 1)
 
     # 2. windowing into frames of 512 samples, overlapping 
-    windowed = tf.stack([audio[:, i:i+512] for i in range(0,size-320,320)],1)
+    windowed = tf.stack([audio[:, i:i+frame_length] for i in range(0, size-320, 320)], 1)
 
-    window = np.hamming(512)
+    window = np.hamming(frame_length)
     windowed = windowed * window
 
     # 3. Take the FFT to convert to frequency space
-    ffted = tf.spectral.rfft(windowed, [512])
-    ffted = 1.0 / 512 * tf.square(tf.abs(ffted))
+    ffted = tf.spectral.rfft(windowed, [frame_length])
+    ffted = 1.0 / frame_length * tf.square(tf.abs(ffted))
 
     # 4. Compute the Mel windowing of the FFT
-    energy = tf.reduce_sum(ffted,axis=2)+np.finfo(float).eps
+    energy = tf.reduce_sum(ffted,axis=2) + np.finfo(float).eps
     filters = np.load("filterbanks.npy").T
     feat = tf.matmul(ffted, np.array([filters]*batch_size,dtype=np.float32))+np.finfo(float).eps
 
     # 5. Take the DCT again, because why not
     feat = tf.log(feat)
-    feat = tf.spectral.dct(feat, type=2, norm='ortho')[:,:,:26]
+    feat = tf.spectral.dct(feat, type=2, norm='ortho')[:,:,:num_mfcc_features]
 
     # 6. Amplify high frequencies for some reason
-    _,nframes,ncoeff = feat.get_shape().as_list()
+    _, nframes, ncoeff = feat.get_shape().as_list()
     n = np.arange(ncoeff)
     lift = 1 + (22/2.)*np.sin(np.pi*n/22)
     feat = lift*feat
@@ -62,7 +64,7 @@ def compute_mfcc(audio, **kwargs):
 
 
     # 7. And now stick log energy next to the features
-    feat = tf.concat((tf.reshape(tf.log(energy),(-1,width,1)), feat[:, :, 1:]), axis=2)
+    feat = tf.concat( (tf.reshape(tf.log(energy), (-1, width, 1)), feat[:, :, 1:]), axis=2)
 
     return feat
 
@@ -80,6 +82,7 @@ def get_logits(new_input, length, first=[]):
     batch_size = new_input.get_shape()[0]
     context_width = 9 # number of frames in the context; default n_context in DeepSpeech
     num_mfcc_features = 26 # default n_input in DeepSpeech
+    window_width = 2 * context_width + 1 # number of frames on both sides + ourselves
 
     # 1. Compute the MFCCs for the input audio
     # (this is differentable with our implementation above)
@@ -89,7 +92,6 @@ def get_logits(new_input, length, first=[]):
 
     # 2. create overlapping windows and
     # remove dummy depth dimension and reshape into [batch_size, n_windows, window_width, num_mfcc_feat]
-    window_width = 2 * context_width + 1 # number of frames on both sides + ourselves
     features = tf.reshape(features, [new_input.get_shape()[0], -1])
     features = tf.stack([features[:, i:i+window_width*num_mfcc_features]
                             for i in range(0, features.shape[1] - window_width*num_mfcc_features+1, num_mfcc_features)],1)
